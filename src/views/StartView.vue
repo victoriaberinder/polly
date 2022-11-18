3676d292624f0f2d034ae051aed191285faee4fe
laaksoharju <mikael.laaksoharju@it.uu.se>
2022-11-18 10:36:11 +0100

    Trevligt meddelande

--- src/views/StartView.vue
+++ src/views/StartView.vue
@@ -11,7 +11,8 @@
     <a href="">Pricing</a>
     <a href="">About</a>
     <a href="">FAQ</a>
-  </ResponsiveNav>f
+  </ResponsiveNav>
+  <h1>Welcome!</h1>
   <label>
     Write poll id: 
     <input type="text" v-model="id">
