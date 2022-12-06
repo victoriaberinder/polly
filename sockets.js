5d219c4030d2711826e532f03fe32c9c957b8568
victoriaberinder <victoria.berinder@gmail.com>
2022-12-06 14:56:19 +0100

    createview

--- sockets.js
+++ sockets.js
@@ -1,4 +1,6 @@
-function sockets(io, socket, data) {
+const { text } = require("express");
+
+function sockets(io, socket, data, document, survey_options) {
   socket.emit('init', data.getUILabels());
   
   socket.on('pageLoaded', function (lang) {
@@ -43,7 +45,57 @@ function sockets(io, socket, data) {
     data = new Data();
     data.initializeData();
   })
+
+  var survey_options = document.getElementById('survey_options');
+  var add_more_fields = document.getElementById('add_more_fields');
+  var remove_fields = document.getElementById('remove_fields');
+  
+  add_more_fields.onclick = function(){
+    var newField = document.createElement('input');
+    newField.setAttribute('type','text');
+    newField.setAttribute('name','survey_options[]');
+    newField.setAttribute('class','survey_options');
+    newField.setAttribute('siz',50);
+    newField.setAttribute('placeholder','Another Field');
+    survey_options.appendChild(newField);
+  }
+  
+  remove_fields.onclick = function(){
+    var input_tags = survey_options.getElementsByTagName('input');
+    if(input_tags.length > 2) {
+      survey_options.removeChild(input_tags[(input_tags.length) - 1]);
+    }
+  }
+  
+  document.getElementById('print-values-btn').onclick = function() {
+    let allTextBoxes = document.getElementsByName('survey_options[]');  
+    for(let i of allTextBoxes){
+        console.log(i.value) //here you will be able to see all values in the console
+    }
+  }
+  
+  module.exports = sockets;
  
 }
 
-module.exports = sockets;
+const addBtn = document.querySelector(".add");
+
+const input = document.querySelector(".inp-group");
+
+function addInput(){
+  const name = document.createElement("input");
+  name.type="text";
+  name.placeholder="Enter your name";
+
+  const email = document.createElement("input");
+  email.type="text";
+  name.placeholder="Enter your Email";
+
+  const btn=document.createElement("a");
+  btn.className = "delete";
+  btn.innerHTML = "&times";
+}
+
+addBtn.addEventListener("click", addInput);
+
+
--- src/views/CreateView.vue
+++ src/views/CreateView.vue
@@ -1,97 +1,67 @@
 <template>
-    <div>
-      Poll link: 
-      <input type="text" v-model="pollId">
-      <button v-on:click="createPoll">
-        Create poll
-      </button>
-      <div>
-        {{uiLabels.question}}:
-        <input type="text" v-model="question">
-        <div>
-          Answers:
-          <input v-for="(_, i) in answers" 
-                 v-model="answers[i]" 
-                 v-bind:key="'answer'+i">
-          <button v-on:click="addAnswer">
-            Add answer alternative
-          </button>
-        </div>
-      </div>
-      <button v-on:click="addQuestion">
-        Add question
-      </button>
-      <input type="number" v-model="questionNumber">
-      <button v-on:click="runQuestion">
-        Run question
-      </button>
-      {{data}}
-      <router-link v-bind:to="'/result/'+pollId">Check result</router-link>
-    </div>
-      <div class="wrapper">
-      <button class="exitbutton" @click="$router.push('/')">Exit</button>
-      </div>
-  </template>
-  
-  <script>
-  import io from 'socket.io-client';
-  const socket = io();
-  
-  export default {
-    name: 'CreateView',
-    data: function () {
-      return {
-        lang: "",
-        pollId: "",
-        question: "",
-        answers: ["", ""],
-        questionNumber: 0,
-        data: {},
-        uiLabels: {}
-      }
-    },
-    created: function () {
-      this.lang = this.$route.params.lang;
-      socket.emit("pageLoaded", this.lang);
-      socket.on("init", (labels) => {
-        this.uiLabels = labels
-      })
-      socket.on("dataUpdate", (data) =>
-        this.data = data
-      )
-      socket.on("pollCreated", (data) =>
-        this.data = data)
-    },
-    methods: {
-      createPoll: function () {
-        socket.emit("createPoll", {pollId: this.pollId, lang: this.lang })
-      },
-      addQuestion: function () {
-        socket.emit("addQuestion", {pollId: this.pollId, q: this.question, a: this.answers } )
-      },
-      addAnswer: function () {
-        this.answers.push("");
-      },
-      runQuestion: function () {
-        socket.emit("runQuestion", {pollId: this.pollId, questionNumber: this.questionNumber})
-      }
-    }
-  }
-  </script>
-  
-  <style>
-    .exitbutton {
-      width: 5rem;
-      height: 2rem;
-      border-radius: 15px;
-      border-color: rgb(227, 123, 123);
-      margin: 2.5rem;
-      color: white;
-      background-color: rgb(235, 76, 76);
-      position: absolute;
-      bottom: 0;
-      left: 0;
-      font-size: 15px; 
-    }
-  </style>
-  
+<body>
+  <div class="container">
+    <div class="wrap">
+      <h1> Add Dynamic Table</h1>
+      <a href="#" class="add">&plus;</a>
+  </div>
+  <div class="inp-group">
+  </div>
+  </div>
+</body>
+
+</template>
+
+<script >
+
+</script>
+
+<style>
+
+*{
+  margin:0;
+  padding:0;
+  box-sizing: border-box;
+}
+body {
+  width: 100%;
+  height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  background: linear-gradient(to top right, white 0%);
+}
+.container{
+  max-width: 600px;
+  background: grey;
+  border-radius: 5px;
+  width: 600px;
+  padding: 20px;
+  box-shadow: 0 2px 2px 3px black;
+}
+.wrap{
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  width: 100%;
+  margin-bottom: 40px;
+  padding-bottom: 10px;
+  border-bottom: 2px solid #ccc;
+}
+
+.add{
+  text-decoration: none;
+  display: inline-block;
+  width: 30px;
+  height: 30px;
+  background: pink;
+  font-size: 2rem;
+  font-weight: bold;
+  color: white;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+
+}
+
+</style>
