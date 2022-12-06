a2f86317268ad0ac054815730eaaa59f0ce0d73e
sagaedstam <117369469+sagaedstam@users.noreply.github.com>
2022-12-06 13:50:07 +0100

    NYA NYA

---
+++ src/views/OldCreateView.vue
@@ -0,0 +1,96 @@
+<template>
+  <div>
+    Poll link: 
+    <input type="text" v-model="pollId">
+    <button v-on:click="createPoll">
+      Create poll
+    </button>
+    <div>
+      {{uiLabels.question}}:
+      <input type="text" v-model="question">
+      <div>
+        Answers:
+        <input v-for="(_, i) in answers" 
+               v-model="answers[i]" 
+               v-bind:key="'answer'+i">
+        <button v-on:click="addAnswer">
+          Add answer alternative
+        </button>
+      </div>
+    </div>
+    <button v-on:click="addQuestion">
+      Add question
+    </button>
+    <input type="number" v-model="questionNumber">
+    <button v-on:click="runQuestion">
+      Run question
+    </button>
+    {{data}}
+    <router-link v-bind:to="'/result/'+pollId">Check result</router-link>
+  </div>
+    <div class="wrapper">
+    <button class="exitbutton" @click="$router.push('/')">Exit</button>
+    </div>
+</template>
+
+<script>
+import io from 'socket.io-client';
+const socket = io();
+
+export default {
+  name: 'CreateView',
+  data: function () {
+    return {
+      lang: "",
+      pollId: "",
+      question: "",
+      answers: ["", ""],
+      questionNumber: 0,
+      data: {},
+      uiLabels: {}
+    }
+  },
+  created: function () {
+    this.lang = this.$route.params.lang;
+    socket.emit("pageLoaded", this.lang);
+    socket.on("init", (labels) => {
+      this.uiLabels = labels
+    })
+    socket.on("dataUpdate", (data) =>
+      this.data = data
+    )
+    socket.on("pollCreated", (data) =>
+      this.data = data)
+  },
+  methods: {
+    createPoll: function () {
+      socket.emit("createPoll", {pollId: this.pollId, lang: this.lang })
+    },
+    addQuestion: function () {
+      socket.emit("addQuestion", {pollId: this.pollId, q: this.question, a: this.answers } )
+    },
+    addAnswer: function () {
+      this.answers.push("");
+    },
+    runQuestion: function () {
+      socket.emit("runQuestion", {pollId: this.pollId, questionNumber: this.questionNumber})
+    }
+  }
+}
+</script>
+
+<style>
+  .exitbutton {
+    width: 5rem;
+    height: 2rem;
+    border-radius: 15px;
+    border-color: rgb(227, 123, 123);
+    margin: 2.5rem;
+    color: white;
+    background-color: rgb(235, 76, 76);
+    position: absolute;
+    bottom: 0;
+    left: 0;
+    font-size: 15px; 
+  }
+</style>
---
+++ src/views/OldPlayView.vue
@@ -0,0 +1,102 @@
+<template>
+    <body>
+    <header>
+    <div class="logo"><img src="/img/logo.png">Quiz library</div>
+  </header>
+    <div>
+      Play link: 
+      <input type="text" v-model="playId">
+      <button v-on:click="playCross">
+        Create cross
+      </button>
+    </div>
+    <div class="wrapper">
+    <button class="exitbutton" @click="$router.push('/')">Exit</button>
+    </div>
+    <table class="center">
+  <tbody>
+    <tr>
+      <td>Demo</td>
+      <td>Play</td>
+    </tr>
+    <tr>
+      <td>Djur</td>
+      <td>Play</td>
+    </tr>
+    <tr>
+      <td>Huvudstäder</td>
+      <td>PLay</td>
+    </tr>
+    
+  </tbody>
+</table>
+    
+</body>
+  </template>
+  
+  <script>
+  /*import io from 'socket.io-client'; 
+  const socket = io();*/
+  
+  </script>
+
+  <style>
+ header {
+    background-color: rgb(194, 241, 255);
+    width: 100%;
+    height: 33%;
+  }
+  
+
+  .logo {
+    text-transform: uppercase;
+    letter-spacing: 0.25em;
+    font-size: 2.5rem;
+    color: white;
+    padding-top:0.2em;
+    text-align: center;
+  }
+
+  .logo img {
+    height:2.5rem;
+    vertical-align: bottom;
+    margin-right: 0.5rem; 
+  }
+
+  .exitbutton {
+    width: 5rem;
+    height: 2rem;
+    border-radius: 15px;
+    border-color: rgb(227, 123, 123);
+    margin: 2.5rem;
+    color: white;
+    background-color: rgb(235, 76, 76);
+    position: absolute;
+    bottom: 0;
+    left: 0;
+    font-size: 15px; 
+  }
+
+  .quizlibary{
+    width: 100px;
+    height: 300px;
+    border-radius: 15px;
+    border-color: aquamarine;
+    margin: 2.5rem;
+  }
+
+table { 
+    border-collapse: collapse; 
+    width:100%;
+}
+
+tr + tr > td{
+  border-top: 2px solid #dee2e6;
+}
+
+.center{
+  margin-right: 1000px;
+  margin-top: 10%;
+}
+
+</style>
