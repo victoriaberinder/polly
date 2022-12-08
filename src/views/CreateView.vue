367e921526e1b9808d441c1e611b90dab8791612
victoriaberinder <victoria.berinder@gmail.com>
2022-12-08 15:11:21 +0100

    create

--- src/views/CreateView.vue
+++ src/views/CreateView.vue
@@ -1,67 +1,130 @@
 <template>
-<body>
-  <div class="container">
-    <div class="wrap">
-      <h1> Add Dynamic Table</h1>
-      <a href="#" class="add">&plus;</a>
-  </div>
-  <div class="inp-group">
-  </div>
-  </div>
-</body>
+  <body>
+    <div class="container">
+      <div class="wrap">
+        <h1> Add Dynamic Table</h1>
+        <a href="/addInput" class="add">&plus;</a>
+    </div>
+    <div class="inp-group">
+    </div>
+    </div>
+  </body>
+  
+  </template>
+  
+  <script >
+import io from 'socket.io-client';
+const socket = io();
 
-</template>
+const addBtn = document.querySelector(".add");
+const input = document.querySelector(".inp-group");
 
-<script >
+export default {
+  name: 'CreateView',
+  data: function () {
+    return {
+      lang: "",
+      pollId: "",
+      question: "",
+      answers: ["", ""],
+      questionNumber: 0,
+      data: {},
+      uiLabels: {}
+    }
+  },
 
-</script>
+  created: function () {
+    this.lang = this.$route.params.lang;
+    socket.emit("pageLoaded", this.lang);
+    socket.on("init", (labels) => {
+      this.uiLabels = labels
+    })
+    socket.on("dataUpdate", (data) =>
+      this.data = data
+    )
+    socket.on("pollCreated", (data) =>
+      this.data = data)
+  },
 
-<style>
 
-*{
-  margin:0;
-  padding:0;
-  box-sizing: border-box;
+
+removeInput:function (){
+  this.parentElement.remove();
+  socket.emit("removeInput", {pollId: this.removeInput, lang: this.lang })
+},
+
+addInput:function (){
+  socket.emit("addInput", {pollId: this.addInput, lang: this.lang })
+  const name = document.createElement("input");
+  name.type="text";
+  name.placeholder="Enter your name";
+
+  const email = document.createElement("input");
+  email.type="text";
+  name.placeholder="Enter your Email";
+
+  const btn=document.createElement("a");
+  btn.className = "delete";
+  btn.innerHTML = "&times";
+
+  btn.addEventListener("click", this.removeInput);
+
+  const flex=document.createElement("div");
+  flex.className="flex";
+
+  input.appendChild(flex);
+  flex.appendChild(name);
+  flex.appendChild(email);
+  flex.appendChild(btn);
+
+  addBtn.addEventListener("click", this.addInput);
 }
-body {
-  width: 100%;
-  height: 100vh;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  background: linear-gradient(to top right, white 0%);
-}
-.container{
-  max-width: 600px;
-  background: grey;
-  border-radius: 5px;
-  width: 600px;
-  padding: 20px;
-  box-shadow: 0 2px 2px 3px black;
-}
-.wrap{
-  display: flex;
-  justify-content: space-between;
-  align-items: center;
-  width: 100%;
-  margin-bottom: 40px;
-  padding-bottom: 10px;
-  border-bottom: 2px solid #ccc;
-}
-
-.add{
-  text-decoration: none;
-  display: inline-block;
-  width: 30px;
-  height: 30px;
-  background: pink;
-  font-size: 2rem;
-  font-weight: bold;
-  color: white;
-  display: flex;
-  justify-content: center;
-  align-items: center;
 
 }
-
-</style>
+  </script>
+  
+  <style>
+  *{
+    margin:0;
+    padding:0;
+    box-sizing: border-box;
+  }
+  body {
+    width: 100%;
+    height: 100vh;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    background: linear-gradient(to top right, white 0%);
+  }
+  .container{
+    max-width: 600px;
+    background: grey;
+    border-radius: 5px;
+    width: 600px;
+    padding: 20px;
+    box-shadow: 0 2px 2px 3px black;
+  }
+  .wrap{
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    width: 100%;
+    margin-bottom: 40px;
+    padding-bottom: 10px;
+    border-bottom: 2px solid #ccc;
+  }
+  .add{
+    text-decoration: none;
+    display: inline-block;
+    width: 30px;
+    height: 30px;
+    background: pink;
+    font-size: 2rem;
+    font-weight: bold;
+    color: white;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+  }
+  </style>
