d1cb3c53239be97a6eb82b306d9680424ee52617
Sofia Pettersson <sofiapettersson@student-212-140.eduroam.uu.se>
2023-01-10 12:23:15 +0100

    knappar

--- Data.js
+++ Data.js
@@ -164,8 +164,4 @@ Data.prototype.getMyResult = function (quizId, user) {
   return this.quizes[quizId].users[user]
 }
 
-<<<<<<< HEAD
 module.exports = Data;
-=======
-module.exports = Data;
->>>>>>> c7219fb52463261e6d8b59d52688fec63a4a971a
