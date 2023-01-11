98e3f80136396fa4f74650ffb79b962dcf4b107a
sagaedstam <117369469+sagaedstam@users.noreply.github.com>
2023-01-11 16:11:37 +0100

    NYTT CSS

--- src/views/FinalResultView.vue
+++ src/views/FinalResultView.vue
@@ -247,114 +247,7 @@ body {
   height: 10px;
   border-radius: 50%;
   box-shadow:
-    -120px -220px #b3e0ff,
-    250px -20px #c5f5e5,
-    190px 20px #a9d9ff,
-    -110px -310px #ffc6e8,
-    -110px -290px #fff4cc,
-    -50px -310px #ffcce0,
-    230px -30px #ffd9cc,
-    180px -350px #ffc4f2,
-    -10px -340px #9ff9ff,
-    220px -390px #c9ff99,
-    -70px -30px #ffc8d9,
-    -110px -340px #c1c9ff,
-    155px -240px #7df0ff,
-    -150px -380px #9fffdb,
-    -50px -40px #7fffeb,
-    -95px -175px #d3ff99,
-    -90px 10px #67a9ff,
-    110px -310px #80dfff,
-    70px -415px #ff99e6,
-    170px -100px #ff7cb3,
-    -245px 25px #ffcc99,
-    100px -325px #ff99b3,
-    -210px -180px #a8ffe6,
-    240px -130px #e6a8ff,
-    140px -200px #db73ff,
-    125px -175px #7ed9ff,
-    120px -380px #ff7da3,
-    145px -110px #ffc899,
-    35px -80px #ff99e6,
-    -60px -200px #cf73ff,
-    -200px -230px #e6ff99,
-    -130px -380px #f299ff,
-    -40px -410px #ff99e6,
-    210px -110px #a3fff2,
-    90px -280px #80cfff,
-    -20px -190px #d5ff99,
-    140px -390px #c9ff99,
-    60px -3px #7dccff,
-    -160px -280px #f299ff,
-    -160px -230px #9bfff7,
-    -240px -350px #7dffb9,
-    60px -360px #80cfff,
-    245px -170px #7dc6ff,
-    225px -140px #e6a3ff,
-    140px -210px #db73ff,
-    210px -285px #ffcc99,
-    180px -130px #73a8ff,
-    90px -120px #cf73ff,
-    190px 70px #7df0ff,
-    -20px -380px #7dff80,
-    100px -10px #ff80d1,
-    20px 30px #f299ff,
-    300px 30px #ffcc99,
-
-    /* FLER POS*/
-
-    10px -300px #b3e0ff,
-    -50px -350px #c5f5e5,
-    -30px -50px #a9d9ff,
-    -70px -200px #ffc6e8,
-    -80px -450px #fff4cc,
-    -90px -100px #ffcce0,
-    100px 250px #ffd9cc,
-    -110px 300px #ffc4f2,
-    -120px -150px #9ff9ff,
-    -130px 400px #c9ff99,
-    140px 250px #ffc8d9,
-    150px -350px #c1c9ff,
-    -160px 200px #7df0ff,
-    170px -450px #9fffdb,
-    -180px 100px #7fffeb,
-    190px -275px #d3ff99,
-    200px 50px #67a9ff,
-    210px 310px #80dfff,
-    220px -415px #ff99e6,
-    -230px 150px #ff7cb3,
-    240px -25px #ffcc99,
-    -250px 325px #ff99b3,
-    260px -180px #a8ffe6,
-    270px 130px #e6a8ff,
-    -280px -200px #db73ff,
-    290px 175px #7ed9ff,
-    -300px -380px #ff7da3,
-    -310px 110px #ffc899,
-    -320px -80px #ff99e6,
-    330px 200px #cf73ff,
-    -340px -230px #e6ff99,
-    350px 380px #f299ff,
-    360px -410px #ff99e6,
-    370px 110px #a3fff2,
-    -380px -280px #80cfff,
-    -390px 190px #d5ff99,
-    400px -390px #c9ff99,
-    -410px 3px #7dccff,
-    420px -280px #f299ff,
-    -430px 230px #9bfff7,
-    -440px -350px #7dffb9,
-    450px -360px #80cfff,
-    -460px 170px #7dc6ff,
-    470px -140px #e6a3ff,
-    480px 210px #db73ff,
-    -490px -285px #ffcc99,
-    500px -130px #73a8ff,
-    -510px 120px #cf73ff,
-    520px -70px #7df0ff,
-    -530px 380px #7dff80,
-    540px 10px #ff80d1;
-    
+    -120px -220px #b3e0ff,250px -20px #c5f5e5,190px 20px #a9d9ff,-110px -310px #ffc6e8,-110px -290px #fff4cc,-50px -310px #ffcce0,230px -30px #ffd9cc,180px -350px #ffc4f2,-10px -340px #9ff9ff,220px -390px #c9ff99,-70px -30px #ffc8d9,-110px -340px #c1c9ff,155px -240px #7df0ff,-150px -380px #9fffdb,-50px -40px #7fffeb,-95px -175px #d3ff99,-90px 10px #67a9ff,110px -310px #80dfff,70px -415px #ff99e6,170px -100px #ff7cb3,-245px 25px #ffcc99,100px -325px #ff99b3,-210px -180px #a8ffe6,240px -130px #e6a8ff,140px -200px #db73ff,125px -175px #7ed9ff,120px -380px #ff7da3,145px -110px #ffc899,35px -80px #ff99e6,-60px -200px #cf73ff,-200px -230px #e6ff99,-130px -380px #f299ff,-40px -410px #ff99e6,210px -110px #a3fff2,90px -280px #80cfff,-20px -190px #d5ff99,140px -390px #c9ff99,60px -3px #7dccff,-160px -280px #f299ff,-160px -230px #9bfff7,-240px -350px #7dffb9,60px -360px #80cfff,245px -170px #7dc6ff,225px -140px #e6a3ff,140px -210px #db73ff,210px -285px #ffcc99,180px -130px #73a8ff,90px -120px #cf73ff,190px 70px #7df0ff,-20px -380px #7dff80,100px -10px #ff80d1,20px 30px #f299ff,300px 30px #ffcc99,10px -300px #b3e0ff,-50px -350px #c5f5e5,-30px -50px #a9d9ff,-70px -200px #ffc6e8,-80px -450px #fff4cc,-90px -100px #ffcce0,100px 250px #ffd9cc,-110px 300px #ffc4f2,-120px -150px #9ff9ff,-130px 400px #c9ff99,140px 250px #ffc8d9,150px -350px #c1c9ff,-160px 200px #7df0ff,170px -450px #9fffdb,-180px 100px #7fffeb,190px -275px #d3ff99,200px 50px #67a9ff,210px 310px #80dfff,220px -415px #ff99e6,-230px 150px #ff7cb3,240px -25px #ffcc99,-250px 325px #ff99b3,260px -180px #a8ffe6,270px 130px #e6a8ff,-280px -200px #db73ff,290px 175px #7ed9ff,-300px -380px #ff7da3,-310px 110px #ffc899,-320px -80px #ff99e6,330px 200px #cf73ff,-340px -230px #e6ff99,350px 380px #f299ff,360px -410px #ff99e6,370px 110px #a3fff2,-380px -280px #80cfff,-390px 190px #d5ff99,400px -390px #c9ff99,-410px 3px #7dccff,420px -280px #f299ff,-430px 230px #9bfff7,-440px -350px #7dffb9,450px -360px #80cfff,-460px 170px #7dc6ff,470px -140px #e6a3ff,480px 210px #db73ff,-490px -285px #ffcc99,500px -130px #73a8ff,-510px 120px #cf73ff,520px -70px #7df0ff,-530px 380px #7dff80,540px 10px #ff80d1; 
     animation: explosion 1s ease-out infinite backwards, grav 1s ease-in infinite backwards, pos 5s linear infinite backwards;
 }
 
@@ -364,44 +257,17 @@ body {
 }
 
 @keyframes explosion {
-  from {
-    box-shadow: 0 0 white
-  }
+  from {box-shadow: 0 0 white}
 }
-
 @keyframes grav {
-  to {
-    transform: translateY(220px);
-    opacity: 0;
-  }
+  to {transform: translateY(220px);opacity: 0;}
 }
-
 @keyframes pos {
-
-  0%,
-  20% {
-    margin: 10% 40%;
-  }
-
-  21%,
-  40% {
-    margin: 40% 30%;
-  }
-
-  41%,
-  60% {
-    margin: 20% 70%;
-  }
-
-  61%,
-  80% {
-    margin: 30% 20%;
-  }
-
-  81%,
-  100% {
-    margin: 30% 80%;
-  }
+  0%,20% {margin: 10% 40%;}
+  21%,40% {margin: 40% 30%;}
+  41%,60% {margin: 20% 70%;}
+  61%,80% {margin: 30% 20%;}
+  81%,100% {margin: 30% 80%;}
 }
 
 @media only screen and (max-width: 750px) {
@@ -417,7 +283,5 @@ body {
     width: 70vw;
     height: auto;
   }
-
-
 }
 </style>
